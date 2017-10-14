266b1f8803d436d4b5b649266649df5d0317be2a
José Carlos Yebes Torres <jcyebes@eservicios.indra.es>
2017-10-14 12:14:06 +0200

    Fisrt commit

---
+++ itunesAPIVIPER/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  itunesAPIVIPER
+//
+//  Created by José Carlos Yebes Torres on 14/10/17.
+//  Copyright © 2017 José Carlos Yebes Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
